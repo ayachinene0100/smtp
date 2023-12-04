2c2065114b23fccc34678b0a224e195e9658f817
ayachinene0100 <ayachinene0100@163.com>
2023-12-04 16:21:39 +0800

    优化代码

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def split(
     tar_pattern = splitext(tar_path)[0] + '.z*'
     files = glob(tar_pattern)
     if not files:
-        run(['zip', '-s', f'{split_size}m', tar_path, src])
+        run(['zip', '-j', '-s', f'{split_size}m', tar_path, src])
         files = glob(tar_pattern)
     return sorted(files)
 
@@ -36,12 +36,22 @@ smtp.receivers = 'qwe@qq.com'
 
 src = 'C:\\a.exe'
 
-if __name__ == '__main__':
-    output = split(src)
-    for o in output:
-        print(f'Sending {o} ...', end=' ')
+def send(output: list[str], begin: int = None, end: int = None, ids_: list[int] = None) -> None:
+    begin = begin or 1
+    end = end or len(output)
+    ids_ = ids_ or range(1, len(output) + 1)
+    for i in range(begin, end + 1):
+        if i not in ids_:
+            continue
+
+        print(f'Sending {output[i - 1]} ...', end=' ')
         smtp.send(
             subject=' ',
-            attachments={basename(o): Path(o)}
+            attachments={basename(output[i - 1]): Path(output[i - 1])}
         )
         print('Done')
+
+
+if __name__ == '__main__':
+    output_ = split(src)
+    send(output_)
